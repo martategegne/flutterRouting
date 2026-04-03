caad0394b59ccfa84e5e89b6d1a7408663d13b85
martategegne <mirandatefl2023@gmail.com>
2026-04-03 21:52:23 +0300

    syntax error corrected for the second time

--- go_routing_basic_routing/lib/main.dart
+++ go_routing_basic_routing/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() =>
+  runApp( const MyApp());
+
 
 final  _router = GoRouter(
   routes: [
